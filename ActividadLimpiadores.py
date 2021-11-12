0640054ead424d34b5d881a2a57194f9f2bb6176
luisferro2 <a01378248@itesm.mx>
2021-11-11 23:09:28 -0600

    Agregar métodos importantes.
    Agregar métodos step, recibir porcentaje de celdas, número de pasos de ejecución, y número de pasos que se llevan hasta ahora en el método step del modelo.

--- ActividadLimpiadores.py
+++ ActividadLimpiadores.py
@@ -4,36 +4,50 @@ from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
 
 class LimpiadorAgent(Agent):
-    """ An agent with fixed initial wealth."""
+    """ An agent that cleans dirty cells."""
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
+
+    def step(self):
+        # Revisar si la celda está sucia o limpia.
+        pass
 
 class CeldaAgent(Agent):
     """ An agent with fixed initial wealth."""
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
 
+    def step(self):
+        pass
+
+
 class LimpiadoresModel(Model):
     """A model with some number of agents."""
-    def __init__(self, N_limpiadores, N_celdas, width, height):
+    def __init__(self, N_limpiadores, percentage_dirty: float, max_steps, width, height):
         self.num_agents = N_limpiadores
-        self.num_celdas = N_celdas
+        self.num_celdas = int(width * heigth * percentage_dirty)
+        self.max_steps = max_steps
+        self.current_steps = 0
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
         self.running = True
 
-        # Create agents
-        for i in range(self.num_agents):
+        # Create Cell agents
+        for i in range(self.num_celdas):
             a = CeldaAgent(i, self)
-            self.schedule.add(a)
-            # Add the agent to a random grid cell
+            #self.schedule.add(a) There's no need to activate the 
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(a, (x, y))
 
-        # Create agents
+        # Create cleaning agents
         for i in range(self.num_agents):
             a = LimpiadorAgent(i, self)
             self.schedule.add(a)
             # Add the agent to a random grid cell
             self.grid.place_agent(a, (1, 1))
+
+    def step(self):
+        """ Advance the model by one step"""
+        self.schedule.step()
+        self.current_steps += 1
